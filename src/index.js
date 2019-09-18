e4f7c7b003ca3f4afa02ebf0b527b448f68265c0
Akseli Palén <akseli.palen@gmail.com>
2019-09-19 01:06:48 +0300

    Upgrade to sprinkler v1.0.1 for smoother experience

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-const emojilist = require('./emojilist');
-const Sprinkler = require('sprinkler');
+const emojilist = require('./emojilist')
+const sprinkler = require('sprinkler')
 
-var c = document.getElementById('canvas');
-var s = Sprinkler.create(c);
+var c = document.getElementById('canvas')
+var s = sprinkler.create(c)
 
 var images = emojilist.map(filename => {
-  return 'img/' + filename + '.png';
-});
-s.load(images, function (err, start) {
-  if (err) { console.error(err); return; }
-  start({
-    zMin: 1, zMax: 1,
-    rMin: 0, rMax: 0,
-    aMin: 1, aMax: 1,
-    drMin: -0.02, drMax: +0.02,
-    daMin: 0, daMax: 0,
-    dyMin: 150, dyMax: 200
-  });
-});
+  return 'img/' + filename + '.png'
+})
+
+s.start(images, {
+  burnInSeconds: 10,
+  zMin: 1, zMax: 1,
+  rMin: 0, rMax: 0,
+  aMin: 1, aMax: 1,
+  drMin: -0.02, drMax: +0.02,
+  daMin: 0, daMax: 0,
+  dyMin: 150, dyMax: 200
+})
